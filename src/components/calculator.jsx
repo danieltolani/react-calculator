df14d28acc9222cb91e8c046b4710e19b4855a38
Tolani Daniel <mumytosin2016@gmail.com>
2022-10-11 18:32:19 +0100

    update: Full working arithmetic with no history

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,77 +1,124 @@
-import { useState, useEffect }from "react";
+import { useState, useEffect } from "react";
 
-export default function Calculator(){
+export default function Calculator() {
+  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+  const operations = ["+", "-", "*", "/", "%", "()", "C"];
+  const [currentNum, setCurrentNum] = useState("");
+  const [prevNum, setPrevNum] = useState("");
+  const [selectedOperation, setSelectedOperation] = useState("");
+  const [history, setHistory] = useState("");
 
-    const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
-    const operations = ["+", "-", "*", "/", "%", "( )", "C"];
-    const [currentNum, setCurrentNum] = useState("") 
-    // const [updated, setUpdated] = useState(1)
-    // let currentNum = ""
+  useEffect(() => {
+    if (selectedOperation) {
+      return setHistory(`${prevNum} ${selectedOperation}`);
+    }
+  }, [selectedOperation, prevNum]);
+  // Handle Button Press
+  const pressed = (value) => {
+    // condition for what each button should do
 
-
-    // Handle Button Press
-    const pressed = (value) => {
-        // console.log(value)
-        appendNumber(value);
-        // setCurrentNum(currentNum + value)
-        // setCurrentNum(prevNum => (prevNum + value))
+    if (value === "c") {
+      setCurrentNum("");
+      setHistory("")
+      return;
     }
 
-
-    // Add number to back of numbers
-    const appendNumber = (value) => {
-        // currentNum = currentNum + value;  
-        setCurrentNum(prevNum => (prevNum + value))
-        console.log(currentNum)
+    // if its an equal sign, it should run the 'calculate' function.
+    if (value === "=" || value === "Enter") {
+      return calculate();
     }
 
-    
+    // check if the input value is contained in the operations array
+    // if it does, continue to append
+    if (operations.includes(value)) {
+      return applyOperation(value);
+    }
 
-    return(
-        <section className="calculator-wrapper">
+    if (numbers.includes(value)) {
+      return appendNumber(value);
+    }
+  };
 
-            <div className="input-container">
-                <div className="cache">
-                        <h4 className="cache--text"> 222222 </h4>
-                </div>
-                <input onChange={e => setCurrentNum(e.target.value)} defaultValue={currentNum} className="cal-input" type="disabled" name="calculator-number-input" id="calculator-input" />
-                
-            </div>
+  // Add numbers to back of number
+  const appendNumber = (value) => {
+    if (selectedOperation) {
+      setCurrentNum(value);
+    } else {
+      setCurrentNum((prevNum) => prevNum + value);
+    }
+  };
 
-            <section className="calculator">
+  // apply an operation inBetween numbers
+  const applyOperation = (value) => {
+    setPrevNum(currentNum);
+    // setCurrentNum("")
+    setSelectedOperation(value);
+  };
 
-                <div className="buttons">
-                    <button onClick={() => pressed('c')}>C</button>
-                    <button onClick={() => pressed('()')}>( )</button>
-                    <button onClick={() => pressed('%')}>%</button>
-                    <button onClick={() => pressed('/')}>&#247;</button>
-                            
-                    <button onClick={() => pressed('1')}>1</button>
-                    <button onClick={() => pressed('2')} >2</button>
-                    <button onClick={() => pressed('3')}>3</button>
-                    <button onClick={() => pressed('*')}>&#0215;</button>
-             
-                    <button onClick={() => pressed('4')}>4</button>
-                    <button onClick={() => pressed('5')}>5</button>
-                    <button onClick={() => pressed('6')}>6</button>
-                    <button onClick={() => pressed('+')}>&#43;</button>
-                    
-                    <button onClick={() => pressed('7')}>7</button>
-                    <button onClick={() => pressed('8')}>8</button>
-                    <button onClick={() => pressed('9')}>9</button>
-                    <button onClick={() => pressed('-')}>&#8722;</button>
-     
-                    <button onClick={() => pressed('.')}>.</button>
-                    <button onClick={() => pressed('0')}>0</button>
-                    <button onClick={() => pressed('000')}>000</button>
-                    <button onClick={() => pressed('=')}>&#61;</button> 
+  const calculate = () => {
+    const operationMap = {
+      "*": (a, b) => a * b,
+      "+": (a, b) => a + b,
+      "-": (a, b) => a - b,
+      "%": (a, b) => a % b,
+    };
 
-                </div>    
-            </section>
-        </section>
-        
-    )
+    const operation = operationMap[selectedOperation];
+    if (!operation) {
+      alert(`Operation ${selectedOperation} is not yet supported 🙃`);
+    }
+
+    const result = operation(parseInt(prevNum), parseInt(currentNum));
+    setPrevNum(result);
+    setCurrentNum(result);
+  };
+
+  return (
+    <section className="calculator-wrapper">
+      <div className="input-container">
+        <div className="cache">
+          <h4 className="cache--text"> {history} </h4>
+        </div>
+        <input
+          placeholder="0"
+          onChange={(e) => setCurrentNum(e.target.value)}
+          value={currentNum}
+          defaultValue={currentNum}
+          className="cal-input"
+          type="disabled"
+          name="calculator-number-input"
+          id="calculator-input"
+        />
+      </div>
+
+      <section className="calculator">
+        <div className="buttons">
+          <button onClick={() => pressed("c")}>C</button>
+          <button onClick={() => pressed("()")}>( )</button>
+          <button onClick={() => pressed("%")}>%</button>
+          <button onClick={() => pressed("/")}>&#247;</button>
+
+          <button onClick={() => pressed("1")}>1</button>
+          <button onClick={() => pressed("2")}>2</button>
+          <button onClick={() => pressed("3")}>3</button>
+          <button onClick={() => pressed("*")}>&#0215;</button>
+
+          <button onClick={() => pressed("4")}>4</button>
+          <button onClick={() => pressed("5")}>5</button>
+          <button onClick={() => pressed("6")}>6</button>
+          <button onClick={() => pressed("+")}>&#43;</button>
+
+          <button onClick={() => pressed("7")}>7</button>
+          <button onClick={() => pressed("8")}>8</button>
+          <button onClick={() => pressed("9")}>9</button>
+          <button onClick={() => pressed("-")}>&#8722;</button>
+
+          <button onClick={() => pressed(".")}>.</button>
+          <button onClick={() => pressed("0")}>0</button>
+          <button onClick={() => pressed("000")}>000</button>
+          <button onClick={() => pressed("=")}>&#61;</button>
+        </div>
+      </section>
+    </section>
+  );
 }
-
-
-
